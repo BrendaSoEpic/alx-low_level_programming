7b73989c2fe7cc82cdccb31d95534f7c98097111
BrendaSoEpic <wogbeijeomab@gmail.com>
2022-07-05 18:10:37 -0700

    Hello, World

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,14 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: the character to be checked
- * Return: 1 if c is a letter, 0 if otherwise
+ * Return: 1 if c is a letter, 0 otherwise
  */
 
 int _isalpha(int c)
 {
 	char ch;
 
-	for (c = 'A'; ch <= 'z'; ch++)
+	for (ch = 'A'; ch <= 'z'; ch++)
 		if (ch == c)
 			return (1);
 	return (0);
