d2586cfb5cbf721faa53cfd7acc28150c6497799
BrendaSoEpic <wogbeijeomab@gmail.com>
2022-07-18 12:56:30 -0700

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: two dimension array to print
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < *; i++)
+	{
+		for (j = 0; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
