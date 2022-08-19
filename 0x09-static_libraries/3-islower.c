37c5d24e19227cb0367013f2cbaec720b3491544
BrendaSoEpic <wogbeijeomab@gmail.com>
2022-08-19 07:20:05 -0700

    delete .101-print_number.c.swp

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	char ch;
+
+	for (ch = 97; ch <= 122; ch++)
+		if (ch == c)
+			return (1);
+	return (0);
+}
